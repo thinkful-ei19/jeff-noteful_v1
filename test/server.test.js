d900683fc53f954617595950065d073aee3dc5b1
Jeff Vega <jeff@intellibliss.com>
2018-03-09 13:58:35 -0800

    installed additional dependencies

--- test/server.test.js
+++ test/server.test.js
@@ -1,15 +1,25 @@
+const app = require('../server');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 
 const expect = chai.expect;
 
+chai.use(chaiHttp);
+
 describe('Reality check', function () {
 
-  it('true should be true', function () {
-    expect(true).to.be.true;
-  });
-
-  it('2 + 2 should equal 4', function () {
-    expect(2 + 2).to.equal(4);
-  });
+  // REMOVED FOR BREVITY
 
 });
+
+// describe('Reality check', function () {
+
+//   it('true should be true', function () {
+//     expect(true).to.be.true;
+//   });
+
+//   it('2 + 2 should equal 4', function () {
+//     expect(2 + 2).to.equal(4);
+//   });
+
+// });
